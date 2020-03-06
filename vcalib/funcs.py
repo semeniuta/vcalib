79c1a664f17d904dd8fd0275adc4ecfa61574670
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-03-06 12:43:01 +0100

    Move suitable existing functions to calibim modulle

--- vcalib/funcs.py
+++ vcalib/funcs.py
@@ -212,69 +212,6 @@ def triangulate_all(calib_runners, runner_prepare):
     return np.array(res)
 
 
-def detect_good_triangulations(res_np, target, tol):
-    
-    bottom = target - tol
-    top = target + tol
-    
-    return (res_np > bottom) & (res_np < top)
-
-
-def find_best_calib_based_on_triangulations(good):
-    
-    ngood_per_calib = np.sum(good, axis=1) 
-    best_calib_run_idx = ngood_per_calib.argmax()
-    good_image_indices_in_best_calib = np.nonzero(good[best_calib_run_idx])[0]
-    
-    return ngood_per_calib, best_calib_run_idx, good_image_indices_in_best_calib
-
-
-def analyze_good_tringulations(good):
-    
-    calib_runs_with_nonzero_good = dict()
-
-    for calib_idx in range(len(good)):
-        calib_metrics = good[calib_idx, :]
-        n_good = np.sum(calib_metrics)
-        if n_good > 0:
-            calib_runs_with_nonzero_good[calib_idx] = calib_metrics
-
-    good_images = dict()
-
-    for k, v in calib_runs_with_nonzero_good.items():
-        
-        nonzero_indices = np.nonzero(v)[0]
-
-        for idx in nonzero_indices:
-            if idx not in good_images:
-                good_images[idx] = 1
-            else:
-                good_images[idx] += 1
-                
-    good_counts = dict()
-    
-    for im_idx, c in good_images.items():
-        
-        if c not in good_counts:
-            good_counts[c] = [im_idx]
-        else:
-            good_counts[c].append(im_idx)
-                
-    return calib_runs_with_nonzero_good, good_counts
-
-
-def select_indices(good_counts, low_threshold=0):
-    
-    selected = [] 
-    
-    counts =  filter(lambda c: c > low_threshold, good_counts.keys())
-    
-    for c in counts:
-        selected += good_counts[c]
-        
-    return selected
-
-
 def pnp_for_each_image_pair(runner_prepare, cm1, dc1, cm2, dc2):
 
     op = runner_prepare['pattern_points']
