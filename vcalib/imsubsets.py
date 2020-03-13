52e07596468fa8d954da177837c60e4eb42fefac
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-03-13 11:45:19 +0100

    Change sample_subsets, subsets_sliding to return NumPy arrays

--- vcalib/imsubsets.py
+++ vcalib/imsubsets.py
@@ -27,28 +27,36 @@ def n_choose_k(n, k):
 
 
 def subsets_sliding(indices, subset_size):
+
+    def gen():
     
-    total = len(indices)
-    
-    if subset_size > total:
-        raise Exception('subset_size should be less or equal to the size of indices')
+        total = len(indices)
         
-    last = total - subset_size
-        
-    for start in range(0, last + 1):
-        yield indices[start:start+subset_size]
+        if subset_size > total:
+            raise Exception('subset_size should be less or equal to the size of indices')
+            
+        last = total - subset_size
+            
+        for start in range(0, last + 1):
+            yield indices[start:start+subset_size]
+
+    return np.array(list(gen()))
 
 
 def sample_subsets(elements, subset_size, n_subsets, seed):
 
-    np.random.seed(seed)
-    seeds = np.random.randint(0, 100 * n_subsets, n_subsets)
+    def gen():
 
-    for s in seeds:
-        np.random.seed(s)
-        subset = np.random.choice(elements, subset_size, replace=False)
+        np.random.seed(seed)
+        seeds = np.random.randint(0, 100 * n_subsets, n_subsets)
 
-        yield subset
+        for s in seeds:
+            np.random.seed(s)
+            subset = np.random.choice(elements, subset_size, replace=False)
+
+            yield subset
+
+    return np.array(list(gen()))
 
 
 def subsets_onehot(subsets, n_images):
